fb28b2d612d895634b832dd4c55bf471c26397f0
aemmazub <mashadulin@gmail.com>
2024-01-18 03:07:38 +0000

    Add: func create dir and files

--- src/01/main.sh
+++ src/01/main.sh
@@ -12,25 +12,45 @@ source ./check.sh
 # echo "$4"
 # echo "$5"
 # echo "$6"
-makeBaseDirName() {
-
-    #НА
-    len=$1
-    symbols=$2
-    DirName=$2
-    while [ "$len" -gt "${#DirName}" ]
-    do
-    length=${#symbols}
-    DirName="$DirName${symbols:length-1:1}"
-    done
+makeBaseName() {
+    len=4
+    symbols=$1
+    DirName=$1
+    while [ "$len" -gt "${#DirName}" ]; do length=${#symbols} DirName="$DirName${symbols:length-1:1}"; done
     echo $DirName
+}
+
+
+createDirAndFiles() {
+    countDir=$1
+    DirName=$2
+    lastSymbol=$3
+    while [ countDir -gt 0]
+    do
+    #Надо добавить еще дату от к названию папки
+    mkdir $DirName && cd $DirName
+    makeFiles $4 $5 $6
+
+    countDir-=1
+    done
+}
+
+makeFiles() {
+    countFiles=$1
+    symbolsFiles=$2
+    sizeFiles=$3
+    BaseNameFile=$(makeBaseName $symbolsFiles)
+    while [ countFiles -gt 0]
+    do
+    head -c "$sizeFiles" < /dev/urandom > $BaseNameFile
+    done
 }
 
 resultCheck=$(Check $1 $2 $3 $4 $5 $6)
 if [[ $resultCheck =~ ^[1]$ ]]; then
     cd $1
-    BaseDirName=$(makeBaseDirName $2 $3)
-    echo $BaseDirName
+    BaseDirName=$(makeBaseName $3)
+
 else
     echo "$resultCheck"
 fi
