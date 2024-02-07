b73f55ece1c810934fa657beb006b35a803b45a9
aemmazub <mashadulin@gmail.com>
2024-02-07 22:23:33 +0000

    09:v01

--- src/09/main.sh
+++ src/09/main.sh
@@ -1,44 +1,20 @@
 #!/bin/bash
 
 while true; do
-#CPU load
-# cpu0=$(awk '/^cpu / {print $2 }' /proc/stat)
-# nice0=$(awk '/^cpu / {print $3 }' /proc/stat)
-# system0=$(awk '/^cpu / {print $4 }' /proc/stat)
-# i0=$(awk '/^cpu / {print $5 }' /proc/stat)
-# sleep 5
-# cpu1=$(awk '/^cpu / {print $2 }' /proc/stat)
-# nice1=$(awk '/^cpu / {print $3 }' /proc/stat)
-# system1=$(awk '/^cpu / {print $4 }' /proc/stat)
-# i1=$(awk '/^cpu / {print $5 }' /proc/stat)
-# cpud=$(($cpu1-$cpu0))
-# niced=$(($nice1-$nice0))
-# systemd=$(($system1-$system0))
-# id=$(($i1-$i0))
-# total=$(($cpud+$niced+$systemd+$id))
-
-avg_load=$(awk '{ print $1 }' /proc/loadavg )
-echo $avg_load
-#memAvailable
-memAvailable=$(awk '/^MemAvailable:/ {print $2}' /proc/meminfo)
-memAvailable=$(echo "scale=2; $memAvailable / 1024 / 1024" | bc -l)
-#Rom
-rom=$(df -h / | awk ' /^\/dev/ { print $4 }'| sed 's/[A-Za-z]*$//')
-
-    echo -e "<html>" >> /var/www/html/index.html
-    echo -e "  <head>" >> /var/www/html/index.html
-    echo -e "  </head>" >> /var/www/html/index.html
-    echo -e "  <body" >> /var/www/html/index.html
-    echo -e  "<p>#  HELP avg_cpu_load</p>" >> /var/www/html/index.html
-    echo -e  "<p># TYPE avg_cpu_load gauge </p>" >> /var/www/html/index.html
-    echo -e  "<p>avg_load $avg_load</p>" >> /var/www/html/index.html
-    echo -e  "<p>#HELP free_RAM</p>" >> /var/www/html/index.html
-    echo -e  "<p>#TYPE free_RAM gauge</p>" >> /var/www/html/index.html
-    echo -e  "<p>nfree_RAM $$memAvailable</p>" >> /var/www/html/index.html
-    echo -e  "<p># HELP free_ROM take </p>" >> /var/www/html/index.html
-    echo -e  "<p># TYPE free_ROM gauge</p>" >> /var/www/html/index.html
-    echo -e  "<p>free_ROM $rom</p>" >> /var/www/html/index.html
-    echo -e "  </body>" >> /var/www/html/index.html
-    echo -e "</html>" >> /var/www/html/index.html
-sleep 5
+    avg_load=$(awk '{ print $1 }' /proc/loadavg )
+    memAvailable=$(awk '/^MemAvailable:/ {print $2}' /proc/meminfo)
+    memAvailable=$(echo "scale=2; $memAvailable / 1024 / 1024" | bc -l)
+    rom=$(df / | awk ' /^\/dev/ { print $4 }'| sed 's/[A-Za-z]*$//')
+    rom=$(echo "scale=2; $rom / 1024 / 1024" | bc -l )
+    echo $rom
+    echo -e  "# HELP avg_cpu_load" > metrics
+    echo -e  "# TYPE avg_cpu_load gauge" >> metrics
+    echo -e  "avg_load $avg_load" >> metrics
+    echo -e  "# HELP free_RAM" >> metrics
+    echo -e  "# TYPE free_RAM gauge" >> metrics
+    echo -e  "free_RAM $memAvailable" >> metrics
+    echo -e  "# HELP free_ROM" >> metrics
+    echo -e  "# TYPE free_ROM gauge" >> metrics
+    echo -e  "free_ROM $rom" >> metrics
+    sleep 5
 done
