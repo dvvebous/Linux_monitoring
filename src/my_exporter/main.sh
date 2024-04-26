d38dd944866e7b13371451ec37aa210660f2ea33
aemmazub <mashadulin@gmail.com>
2024-04-26 21:19:46 +0000

    Add: my_exporter

--- src/my_exporter/main.sh
+++ src/my_exporter/main.sh
@@ -6,15 +6,14 @@ while true; do
     memAvailable=$(echo "scale=2; $memAvailable / 1024 / 1024" | bc -l)
     rom=$(df / | awk ' /^\/dev/ { print $4 }'| sed 's/[A-Za-z]*$//')
     rom=$(echo "scale=2; $rom / 1024 / 1024" | bc -l )
-    echo $rom
-    echo -e  "# HELP avg_cpu_load" > metrics
-    echo -e  "# TYPE avg_cpu_load gauge" >> metrics
-    echo -e  "avg_load $avg_load" >> metrics
-    echo -e  "# HELP free_RAM" >> metrics
-    echo -e  "# TYPE free_RAM gauge" >> metrics
-    echo -e  "free_RAM $memAvailable" >> metrics
-    echo -e  "# HELP free_ROM" >> metrics
-    echo -e  "# TYPE free_ROM gauge" >> metrics
-    echo -e  "free_ROM $rom" >> metrics
+    echo -e  "# HELP avg_cpu_load" > /var/www/html/metrics
+    echo -e  "# TYPE avg_cpu_load gauge" >> /var/www/html/metrics
+    echo -e  "avg_load $avg_load" >> /var/www/html/metrics
+    echo -e  "# HELP free_RAM" >> /var/www/html/metrics
+    echo -e  "# TYPE free_RAM gauge" >> /var/www/html/metrics
+    echo -e  "free_RAM $memAvailable" >> /var/www/html/metrics
+    echo -e  "# HELP free_ROM" >> /var/www/html/metrics
+    echo -e  "# TYPE free_ROM gauge" >> /var/www/html/metrics
+    echo -e  "free_ROM $rom" >> /var/www/html/metrics
     sleep 5
 done
